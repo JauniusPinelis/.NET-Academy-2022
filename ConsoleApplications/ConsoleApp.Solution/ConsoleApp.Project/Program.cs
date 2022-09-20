a326b23bca1ad28fa98149be3c3471302a750e44
JauniusPinelis <JauniusPinelis@gmail.com>
2022-09-20 19:07:07 +0300

    git ignore

---
+++ ConsoleApplications/ConsoleApp.Solution/ConsoleApp.Project/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp.Project.Services;
+
+Console.WriteLine("Hello, World!");
+
+var mainService = new MainService();
+
+while (true)
+{
+    Console.WriteLine("Enter command");
+
+    var command = Console.ReadLine();
+
+    mainService.ExecuteCommand(command);
+}
+
+
