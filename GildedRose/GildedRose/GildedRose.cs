8fb65113d00245f67f119fd97ba2283e4dcb44ef
JauniusPinelis <JauniusPinelis@gmail.com>
2022-10-13 17:19:42 +0300

    Update

---
+++ GildedRose/GildedRose/GildedRose.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (Item item in Items)
+            {
+                if (item.Name == "Sulfuras, Hand of Ragnaros")
+                {
+                    continue;
+                }
+
+                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.Quality > 0)
+                    {
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            item.Quality--;
+                        }
+                    }
+                }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality++;
+
+                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (item.SellIn < 11)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality++;
+                                }
+                            }
+
+                            if (item.SellIn < 6)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality++;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.SellIn--;
+                }
+
+                if (item.SellIn < 0)
+                {
+                    if (item.Name != "Aged Brie")
+                    {
+                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (item.Quality > 0)
+                            {
+                                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                                {
+                                    item.Quality--;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            item.Quality = item.Quality - item.Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
